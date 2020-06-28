12ffa9727275d3039a17914dd65dd8342a9a114b
Mike Burgess <mddburgess@users.noreply.github.com>
2020-06-28 15:46:08 -0700

    Add spring security.

--- backend/src/test/java/com/example/demo/notes/rest/NoteResourceTest.java
+++ backend/src/test/java/com/example/demo/notes/rest/NoteResourceTest.java
@@ -4,18 +4,23 @@ import java.util.List;
 
 import com.example.demo.notes.entity.Note;
 import com.example.demo.notes.repository.NoteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,44 +34,92 @@ public class NoteResourceTest {
     @MockBean
     private NoteRepository repository;
 
-    private Note note = new Note("text");
+    private final Note note = new Note("text");
 
-    @Test
-    void create() throws Exception {
-        given(repository.save(any())).willReturn(note);
-
-        mvc.perform(post("/api/notes")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.text", is("text")));
+    @BeforeEach
+    void beforeEach() {
+        reset(repository);
     }
 
-    @Test
-    void read() throws Exception {
-        given(repository.findAll()).willReturn(List.of(note));
+    @Nested
+    @WithAnonymousUser
+    class Unauthenticated {
+        @Test
+        void create() throws Exception {
+            mvc.perform(post("/api/notes")
+                    .with(csrf()))
+                    .andExpect(status().isUnauthorized());
+            verifyNoInteractions(repository);
+        }
 
-        mvc.perform(get("/api/notes"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].text", is("text")));
+        @Test
+        void read() throws Exception {
+            mvc.perform(get("/api/notes"))
+                    .andExpect(status().isUnauthorized());
+            verifyNoInteractions(repository);
+        }
+
+        @Test
+        void update() throws Exception {
+            mvc.perform(put("/api/notes/1")
+                    .with(csrf()))
+                    .andExpect(status().isUnauthorized());
+            verifyNoInteractions(repository);
+        }
+
+        @Test
+        void remove() throws Exception {
+            mvc.perform(delete("/api/notes/1")
+                    .with(csrf()))
+                    .andExpect(status().isUnauthorized());
+            verifyNoInteractions(repository);
+        }
     }
 
-    @Test
-    void update() throws Exception {
-        given(repository.save(any())).willReturn(note);
+    @Nested
+    @WithMockUser
+    class Authenticated {
 
-        mvc.perform(put("/api/notes/1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.text", is("text")));
-    }
+        @Test
+        void create() throws Exception {
+            given(repository.save(any())).willReturn(note);
 
-    @Test
-    void remove() throws Exception {
-        mvc.perform(delete("/api/notes/1"))
-                .andExpect(status().isOk());
-        verify(repository).deleteById(1L);
+            mvc.perform(post("/api/notes")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{}")
+                    .with(csrf()))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.text", is("text")));
+        }
+
+        @Test
+        void read() throws Exception {
+            given(repository.findAll()).willReturn(List.of(note));
+
+            mvc.perform(get("/api/notes"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$", hasSize(1)))
+                    .andExpect(jsonPath("$[0].text", is("text")));
+        }
+
+        @Test
+        void update() throws Exception {
+            given(repository.save(any())).willReturn(note);
+
+            mvc.perform(put("/api/notes/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{}")
+                    .with(csrf()))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.text", is("text")));
+        }
+
+        @Test
+        void remove() throws Exception {
+            mvc.perform(delete("/api/notes/1")
+                    .with(csrf()))
+                    .andExpect(status().isOk());
+            verify(repository).deleteById(1L);
+        }
     }
 }
